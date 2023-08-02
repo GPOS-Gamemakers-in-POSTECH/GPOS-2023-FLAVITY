90e47fc84bcc3de33339d9967316281789b20e1c
DevMizeKR <96890040+DevMizeKR@users.noreply.github.com>
2023-08-02 16:12:01 +0900

    [Feat] Weak Platform 구현

---
+++ Assets/Scripts/Obstacle/Weak Platform.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeakPlatform : MonoBehaviour
+{
+    [SerializeField] float destroySec = 5f;
+
+    // Collision with Player
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name.Equals("Player"))
+        {
+            Destroy(gameObject, destroySec);
+        }
+    }
+}
